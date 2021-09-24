702230c92bab5fb9f7478e5a5ed8538bafcdda1f
mikhail <>
2021-09-24 12:35:58 +0300

    add chart calls

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let logger = document.getElementById("log");
     logger.textContent += `Calling getPairs...\n`;
     const answer = await browser.run_debot_browser("net.ton.dev", null, null, null, manifest);
     let obj = JSON.parse(answer);
-    //logger.textContent += `success:\nDEBUG: ${JSON.stringify(obj, null, ' ')}\n`;
+   // logger.textContent += `success:\nDEBUG: ${JSON.stringify(obj, null, ' ')}\n`;
     for (let i in obj.tip3tonPairs) {
         logger.textContent += `Name: ${hexToUtf8(obj.tip3tonPairs[i].symbol)} Decimals: ${obj.tip3tonPairs[i].decimals} PairAddr: ${obj.tip3tonPairs[i].tradingPair}\n`;
 
@@ -45,6 +45,62 @@ let logger = document.getElementById("log");
         manifest = buildManifest("getPriceTickHistory", `{"tradingPair":"${pair.tradingPair}","startTime":0}`);
         const chart = await browser.run_debot_browser("net.ton.dev", null, null, null, manifest);
         logger.textContent += chart;
+
+        logger.textContent += "\n\nHistory\n";
+
+        let hst = JSON.parse(chart);
+
+        for(let timeKey in hst.history){
+            logger.textContent += `${timeKey} = `;
+            let date = new Date(0);
+            date.setSeconds(parseInt(timeKey, 10));
+            let timeString = date.toISOString();
+            logger.textContent += `${timeString}\n`;;
+            let dealArray = hst.history[timeKey];
+            for (const deal of dealArray) {
+                logger.textContent += `price: ${hexToUtf8(deal.price)} volume: ${hexToUtf8(deal.volume)} \n`;
+            }
+        }
+
+        logger.textContent += `\n\nCalling getLinearChart...\n`;
+        //1632398400 = September 23, 2021 12:00:00 PM
+        //1632420000 = September 23, 2021 6:00:00 PM
+        manifest = buildManifest("getLinearChart", `{"tradingPair":"${pair.tradingPair}","startTime":1632398400,"endTime":1632420000,"stepTime":3600}`);
+        const linearChart = await browser.run_debot_browser("net.ton.dev", null, null, null, manifest);
+        logger.textContent += linearChart;
+        logger.textContent += "\nLinearChart\n";
+
+        let lchart = JSON.parse(linearChart);
+
+        for(let timeKey in lchart.prices){
+            logger.textContent += `${timeKey} = `;
+            let date = new Date(0);
+            date.setSeconds(parseInt(timeKey, 10));
+            let timeString = date.toISOString();
+            logger.textContent += `${timeString}\n`;;
+            let deal = lchart.prices[timeKey];
+            logger.textContent += `price: ${hexToUtf8(deal.price)} volume: ${hexToUtf8(deal.volume)} \n`;
+        }
+
+        logger.textContent += `\n\nCalling getCandlestickChart...\n`;
+        //1632398400 = September 23, 2021 12:00:00 PM
+        //1632420000 = September 23, 2021 6:00:00 PM
+        manifest = buildManifest("getCandlestickChart", `{"tradingPair":"${pair.tradingPair}","startTime":1632398400,"endTime":1632420000,"stepTime":3600}`);
+        const candlestickChart = await browser.run_debot_browser("net.ton.dev", null, null, null, manifest);
+        logger.textContent += candlestickChart;
+        logger.textContent += "\nCandlestickChart\n";
+
+        let csChart = JSON.parse(candlestickChart);
+
+        for(let timeKey in csChart.candles){
+            logger.textContent += `${timeKey} = `;
+            let date = new Date(0);
+            date.setSeconds(parseInt(timeKey, 10));
+            let timeString = date.toISOString();
+            logger.textContent += `${timeString}\n`;;
+            let candle = csChart.candles[timeKey];
+            logger.textContent += `open: ${hexToUtf8(candle.open)} close: ${hexToUtf8(candle.close)} high: ${hexToUtf8(candle.high)} low: ${hexToUtf8(candle.low)} volume: ${hexToUtf8(candle.volume)} \n`;
+        }
     }
 
     logger.textContent += `\n\nCalling getBuyOrderMsg...\n`;
